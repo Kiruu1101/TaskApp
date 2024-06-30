404c18b4fc07e4475f45ea3d37a1ce1cf3a26c6d
Kiruu1101 <kirangr811@gmail.com>
2024-06-30 20:27:33 +0530

    css

--- frontend/src/assets/styled-components/FormWrapper.js
+++ frontend/src/assets/styled-components/FormWrapper.js
@@ -29,7 +29,7 @@ export const FormWrapper = styled.section`
     }
     &.btn-outline {
       background-color: white;
-      height: 2rem;
+      height: 2.5rem;
       color: #17a2b8;
       border: 1px solid #17a2b8;
       text-align: center;
