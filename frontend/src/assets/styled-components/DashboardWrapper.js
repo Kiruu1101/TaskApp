695e65ef4733aec20a20b5709ae0b5ef46caaf31
Kiruu1101 <kirangr811@gmail.com>
2024-07-03 18:45:01 +0530

    css

--- frontend/src/assets/styled-components/DashboardWrapper.js
+++ frontend/src/assets/styled-components/DashboardWrapper.js
@@ -52,12 +52,12 @@ export const DashboardWrapper = styled.section`
 // Styled components for "Add People" text and pop-ups
 
 export const AddPeopleWrapper = styled.div`
-  display: fixed;
+  display: flex;
   align-items: left;
   color: gray;
   cursor: pointer;
   font-size: 1rem;
-  margin-right: 56rem; /* Adjust spacing as needed */
+  margin-right: 57rem; /* Adjust spacing as needed */
   margin-left: 0.5rem;
 
   svg {
