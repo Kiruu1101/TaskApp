26cca5c7ebad1099dd05e0d6266044450da3709d
Kiruu1101 <kirangr811@gmail.com>
2024-07-03 18:27:17 +0530

    css

--- frontend/src/assets/styled-components/DashboardWrapper.js
+++ frontend/src/assets/styled-components/DashboardWrapper.js
@@ -58,6 +58,7 @@ export const AddPeopleWrapper = styled.div`
   cursor: pointer;
   font-size: 1rem;
   margin-right: 300rem; /* Adjust spacing as needed */
+  margin-left: 0.5rem;
 
   svg {
     margin-right: 0.5rem;
