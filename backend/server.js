cdce82262fca8c26fffe80f6294db1e969af91bd
Kiruu1101 <kirangr811@gmail.com>
2024-07-03 15:38:01 +0530

    resolved server error

--- backend/server.js
+++ backend/server.js
@@ -16,33 +16,36 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 
-
-app.use('/api', emailRoutes);
-
 app.use(cookieParser());
 app.use(express.json());
+
 app.get("/api/health", (req, res) => {
   res.json({
     success: true,
     message: "Your server is alive",
   });
 });
+
 app.use("/api/tasks", taskRouter);
 app.use("/api/users", userRouter);
 app.use('/api/emails', emailRoutes);
 
 
 const __dirname = path.resolve();
+
 app.use("/uploads", express.static("/var/data/uploads"));
+
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 app.get("*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
 );
 app.get("*", (req, res) =>
   res.status(404).json({ message: "Route not found" })
 );
+
 app.use(errorHandlerMiddleware);
-const MONGO_URL="mongodb+srv://kiran:Ranik1101@cluster0.zw5lwa0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" ||process.env.MONGO_URL;
+const MONGO_URL= process.env.MONGO_URL || "mongodb+srv://kiran:Ranik1101@cluster0.zw5lwa0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 try {
   await mongoose.connect(MONGO_URL);
   console.log("db connected");
